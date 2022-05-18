05b9ce38687d8d259ac386cd4df0e7e831bddda7
Krzysiek Wyka <krzysztof@wyka.dev>
2022-05-18 20:55:58 +0200

    Init commit

---
+++ HttpTrigger/index.ts
@@ -0,0 +1,27 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import axios from "axios";
+import * as os from "os";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+) {
+  const hostname = req.query.displayHostname === "true" ? os.hostname() : "";
+
+  const response = await axios.get<{ icon_url: string; value: string }>(
+    "https://api.chucknorris.io/jokes/random"
+  );
+
+  const html = `<html><head><title>Joke</title></head><body style="text-align: center"><h1>Your joke is here</h1>${
+    hostname && "<h2>Hostname: " + hostname + "</h2>"
+  }<table style="margin-left: auto; margin-right: auto;"><tr><td><img src="${
+    response.data.icon_url
+  }" /></td><td>${response.data.value}</td></tr></table></body></html>`;
+
+  context.res = {
+    body: html,
+    headers: { "content-type": "text/html" },
+  };
+};
+
+export default httpTrigger;
